53386d49b873e2e2e4cb31163a46060da76aa2f2
Martin Pool <martin.pool@ing.com>
2021-08-27 10:38:12 +0200

    fix: prettier formatting

--- packages/ajax/src/interceptors/cacheInterceptors.js
+++ packages/ajax/src/interceptors/cacheInterceptors.js
@@ -16,88 +16,87 @@ import {
  * @param {CacheOptions} globalCacheOptions
  * @returns {RequestInterceptor}
  */
-const createCacheRequestInterceptor = (
-  getCacheId,
-  globalCacheOptions,
-) => /** @param {CacheRequest} request */ async request => {
-  validateCacheOptions(request.cacheOptions);
-  const cacheSessionId = getCacheId();
-  resetCacheSession(cacheSessionId); // cacheSessionId is used to bind the cache to the current session
+const createCacheRequestInterceptor =
+  (getCacheId, globalCacheOptions) => /** @param {CacheRequest} request */ async request => {
+    validateCacheOptions(request.cacheOptions);
+    const cacheSessionId = getCacheId();
+    resetCacheSession(cacheSessionId); // cacheSessionId is used to bind the cache to the current session
 
-  const cacheOptions = extendCacheOptions({
-    ...globalCacheOptions,
-    ...request.cacheOptions,
-  });
+    const cacheOptions = extendCacheOptions({
+      ...globalCacheOptions,
+      ...request.cacheOptions,
+    });
 
-  // store cacheOptions and cacheSessionId in the request, to use it in the response interceptor.
-  request.cacheOptions = cacheOptions;
-  request.cacheSessionId = cacheSessionId;
+    // store cacheOptions and cacheSessionId in the request, to use it in the response interceptor.
+    request.cacheOptions = cacheOptions;
+    request.cacheSessionId = cacheSessionId;
 
-  if (!cacheOptions.useCache) {
+    if (!cacheOptions.useCache) {
+      return request;
+    }
+
+    const requestId = cacheOptions.requestIdFunction(request);
+    const isMethodSupported = cacheOptions.methods.includes(request.method.toLowerCase());
+
+    if (!isMethodSupported) {
+      invalidateMatchingCache(requestId, cacheOptions);
+      return request;
+    }
+
+    const pendingRequest = pendingRequestStore.get(requestId);
+    if (pendingRequest) {
+      // there is another concurrent request, wait for it to finish
+      await pendingRequest;
+    }
+
+    const cachedResponse = ajaxCache.get(requestId, cacheOptions.maxAge);
+    if (cachedResponse) {
+      // Return the response from cache
+      request.cacheOptions = request.cacheOptions ?? { useCache: false };
+      /** @type {CacheResponse} */
+      const response = cachedResponse.clone();
+      response.request = request;
+      response.fromCache = true;
+      return response;
+    }
+
+    // Mark this as a pending request, so that concurrent requests can use the response from this request
+    pendingRequestStore.set(requestId);
     return request;
-  }
-
-  const requestId = cacheOptions.requestIdFunction(request);
-  const isMethodSupported = cacheOptions.methods.includes(request.method.toLowerCase());
-
-  if (!isMethodSupported) {
-    invalidateMatchingCache(requestId, cacheOptions);
-    return request;
-  }
-
-  const pendingRequest = pendingRequestStore.get(requestId);
-  if (pendingRequest) {
-    // there is another concurrent request, wait for it to finish
-    await pendingRequest;
-  }
-
-  const cachedResponse = ajaxCache.get(requestId, cacheOptions.maxAge);
-  if (cachedResponse) {
-    // Return the response from cache
-    request.cacheOptions = request.cacheOptions ?? { useCache: false };
-    /** @type {CacheResponse} */
-    const response = cachedResponse.clone();
-    response.request = request;
-    response.fromCache = true;
-    return response;
-  }
-
-  // Mark this as a pending request, so that concurrent requests can use the response from this request
-  pendingRequestStore.set(requestId);
-  return request;
-};
+  };
 
 /**
  * Response interceptor to cache relevant requests
  * @param {CacheOptions} globalCacheOptions
  * @returns {ResponseInterceptor}
  */
-const createCacheResponseInterceptor = globalCacheOptions => /** @param {CacheResponse} response */ async response => {
-  if (!response.request) {
-    throw new Error('Missing request in response');
-  }
-
-  const cacheOptions = extendCacheOptions({
-    ...globalCacheOptions,
-    ...response.request.cacheOptions,
-  });
-
-  const requestId = cacheOptions.requestIdFunction(response.request);
-  const isAlreadyFromCache = !!response.fromCache;
-  const isCacheActive = cacheOptions.useCache;
-  const isMethodSupported = cacheOptions.methods.includes(response.request?.method.toLowerCase());
-
-  if (!isAlreadyFromCache && isCacheActive && isMethodSupported) {
-    if (isCurrentSessionId(response.request.cacheSessionId)) {
-      // Cache the response
-      ajaxCache.set(requestId, response.clone());
+const createCacheResponseInterceptor =
+  globalCacheOptions => /** @param {CacheResponse} response */ async response => {
+    if (!response.request) {
+      throw new Error('Missing request in response');
     }
 
-    // Mark the pending request as resolved
-    pendingRequestStore.resolve(requestId);
-  }
-  return response;
-};
+    const cacheOptions = extendCacheOptions({
+      ...globalCacheOptions,
+      ...response.request.cacheOptions,
+    });
+
+    const requestId = cacheOptions.requestIdFunction(response.request);
+    const isAlreadyFromCache = !!response.fromCache;
+    const isCacheActive = cacheOptions.useCache;
+    const isMethodSupported = cacheOptions.methods.includes(response.request?.method.toLowerCase());
+
+    if (!isAlreadyFromCache && isCacheActive && isMethodSupported) {
+      if (isCurrentSessionId(response.request.cacheSessionId)) {
+        // Cache the response
+        ajaxCache.set(requestId, response.clone());
+      }
+
+      // Mark the pending request as resolved
+      pendingRequestStore.resolve(requestId);
+    }
+    return response;
+  };
 
 /**
  * Response interceptor to cache relevant requests
